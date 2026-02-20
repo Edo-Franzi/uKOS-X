bf6660e2b69eb692195420d359412420bf12fda4
Edo. Franzi <edo.franzi@ukos.ch>
2026-02-20 21:34:06 +0100

    V 0.2.11     - Added a TFL inference model test              - Corrected all the cmns_init()              - Better documentation

--- OS/Includes/uKOS.h
+++ OS/Includes/uKOS.h
@@ -86,8 +86,8 @@
 // -----------------------
 
 #define	uKOS_VERSION_OS			10
-#define	uKOS_VERSION_NUMBER		"0.2.10"
+#define	uKOS_VERSION_NUMBER		"0.2.11"
 #define	uKOS_VERSION_MAJOR		0
 #define	uKOS_VERSION_MINOR		2
-#define	uKOS_VERSION_PATCH		10
+#define	uKOS_VERSION_PATCH		11
 #define	uKOS_VERSION			uKOS_VERSION_NUMBER " " STRG(uKOS_NAME) "\n" STRG(uKOS_OWNER)
