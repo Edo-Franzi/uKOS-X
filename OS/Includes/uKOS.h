d620681588a631a3de202aa26207de0c7846c356
Edo. Franzi <edo.franzi@ukos.ch>
2026-03-10 21:56:29 +0100

    V 0.2.27     - Corrected the privileges.c (arm-v7 & arm-v8)              - New TinyUSB package              - Some typos              - Better documentation

--- OS/Includes/uKOS.h
+++ OS/Includes/uKOS.h
@@ -86,8 +86,8 @@
 // -----------------------
 
 #define	uKOS_VERSION_OS			10
-#define	uKOS_VERSION_NUMBER		"0.2.26"
+#define	uKOS_VERSION_NUMBER		"0.2.27"
 #define	uKOS_VERSION_MAJOR		0
 #define	uKOS_VERSION_MINOR		2
-#define	uKOS_VERSION_PATCH		26
+#define	uKOS_VERSION_PATCH		27
 #define	uKOS_VERSION			uKOS_VERSION_NUMBER " " STRG(uKOS_NAME) "\n" STRG(uKOS_OWNER)
