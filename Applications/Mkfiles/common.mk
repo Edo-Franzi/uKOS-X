cdd441b4a3f216caad1593ce1091c381c76c2cb7
Edo. Franzi <edo.franzi@ukos.ch>
2026-05-21 18:09:22 +0200

    V 0.5.0      - Added the KINT_LEVEL_VERY_HS_PERIPHERALS interruption level for all the targets              - Corrected all the MPU initialisation for M3, M4 and M7 targets              - Improved the model_imager_xyz and the TinyUSB models/managers              - New TinyUSB package              - Imagers & usb set as KINT_LEVEL_VERY_HS_PERIPHERALS              - Modified the video descriptos to be 512 modulo for the image transfers              - Many typos              - Improved the documentation

--- Applications/Mkfiles/common.mk
+++ Applications/Mkfiles/common.mk
@@ -2,6 +2,7 @@
 # =========
 
 # SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2025-2026 Edo. Franzi
 
 #------------------------------------------------------------------------
 # Author:   Edo. Franzi			2023-12-13
